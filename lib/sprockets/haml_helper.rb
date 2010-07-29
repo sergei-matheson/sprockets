360a77d47a92bc4e222661741c76a36c3d8fcfcb
Sergei Matheson <sergeim@pixelinkmedia.com>
2010-07-29 11:42:22 +1000

    Added haml_helper

---
+++ lib/sprockets/haml_helper.rb
@@ -0,0 +1,12 @@
+module Sprockets
+	class HamlHelper
+		def initialize escape_sequence, quote_char
+			@escape_sequence = escape_sequence
+			@quote_char = quote_char
+		end
+
+		def client_js js_string
+			"#{@escape_sequence}+#{js_string.gsub(@quote_char, @escape_sequence)}+#{@escape_sequence}"
+		end
+	end
+end
